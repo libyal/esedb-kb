432570e495217dfc214875d58975e77071592d92
Joachim Metz <joachim.metz@gmail.com>
2015-03-22 12:55:22 +0100

    Worked on catalog script.

--- catalog.py
+++ catalog.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
-# Script to print the ESE database catalog.
-#
-# Copyright (c) 2014, Joachim Metz <joachim.metz@gmail.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""Script to print the ESE database catalog."""
 
 import argparse
 import logging
@@ -25,8 +10,8 @@ import sys
 import pyesedb
 
 
-if pyesedb.get_version() < '20140406':
-  raise ImportWarning('catalog.py requires pyesedb 20140406 or later.')
+if pyesedb.get_version() < u'20140406':
+  raise ImportWarning(u'catalog.py requires pyesedb 20140406 or later.')
 
 
 class ColumnOverlay(object):
@@ -47,7 +32,7 @@ class ColumnOverlay(object):
 
   @property
   def comparable(self):
-    return 'identifier: {0:s}, name: {1:s}, type: {2:s}'.format(
+    return u'identifier: {0:s}, name: {1:s}, type: {2:s}'.format(
         self.column_identifier, self.column_name, self.column_type)
 
 
@@ -71,7 +56,7 @@ class TableOverlay(object):
       column_overlay: the column overlay.
     """
     if column_overlay.comparable in self._column_overlays:
-      raise KeyError('Column overlay: {0:s} already set.'.format(
+      raise KeyError(u'Column overlay: {0:s} already set.'.format(
           column_overlay.comparable))
 
     self._column_overlays[column_overlay.comparable] = column_overlay
@@ -100,8 +85,8 @@ class StdoutWriter(object):
       column_name: the column name.
       column_type: the column type.
     """
-    print '|| {0:d} || {{{{{{ {1:s} }}}}}} || {2:s} ||'.format(
-        column_identifier, column_name, column_type)
+    print(u'|| {0:d} || {{{{{{ {1:s} }}}}}} || {2:s} ||'.format(
+        column_identifier, column_name, column_type))
 
   def WritePageHeader(self, page_summary):
     """Writes the page header to stdout.
@@ -109,14 +94,14 @@ class StdoutWriter(object):
     Args:
       page_summary: the page summary.
     """
-    print '#summary {0:s}'.format(page_summary)
-    print ''
-    print '<wiki:toc max_depth="3" />'
-    print ''
+    print(u'#summary {0:s}'.format(page_summary))
+    print(u'')
+    print(u'<wiki:toc max_depth="3" />')
+    print(u'')
 
   def WriteTableFooter(self):
     """Writes the table footer to stdout."""
-    print ''
+    print(u'')
 
   def WriteTableHeader(self, table_name, template_table_name):
     """Writes the table header to stdout.
@@ -125,12 +110,12 @@ class StdoutWriter(object):
       table_name: the table name.
       template_table_name: the template table name.
     """
-    print '== {{{{{{ {0:s} }}}}}} =='.format(table_name)
+    print(u'== {{{{{{ {0:s} }}}}}} =='.format(table_name))
 
     if template_table_name:
-      print 'Template table: {0:s}'.format(template_table_name)
+      print(u'Template table: {0:s}'.format(template_table_name))
 
-    print '|| *Column indentifier* || *Column name* || *Column type* ||'
+    print(u'|| *Column indentifier* || *Column name* || *Column type* ||')
 
 
 def Main():
@@ -140,19 +125,19 @@ def Main():
     A boolean containing True if successful or False if not.
   """
   args_parser = argparse.ArgumentParser(description=(
-      'Extract the catalog from the ESE database file.'))
+      u'Extract the catalog from the ESE database file.'))
 
   args_parser.add_argument(
-      'source', nargs='?', action='store', metavar='/mnt/c/',
-      default=None, help=('path of the ESE database file.'))
+      u'source', nargs=u'?', action=u'store', metavar=u'/mnt/c/',
+      default=None, help=(u'path of the ESE database file.'))
 
   options = args_parser.parse_args()
 
   if not options.source:
-    print u'Source value is missing.'
-    print u''
+    print(u'Source value is missing.')
+    print(u'')
     args_parser.print_help()
-    print u''
+    print(u'')
     return False
 
   logging.basicConfig(
@@ -161,45 +146,45 @@ def Main():
   output_writer = StdoutWriter()
 
   ESEDB_COLUMN_TYPE_DESCRIPTIONS = {
-      pyesedb.column_types.NULL: 'Null',
-      pyesedb.column_types.BOOLEAN: 'Boolean',
-      pyesedb.column_types.INTEGER_8BIT_UNSIGNED: 'Integer 8-bit unsigned',
-      pyesedb.column_types.INTEGER_16BIT_SIGNED: 'Integer 16-bit signed',
-      pyesedb.column_types.INTEGER_32BIT_SIGNED: 'Integer 32-bit signed',
-      pyesedb.column_types.CURRENCY: 'Currency',
-      pyesedb.column_types.FLOAT_32BIT: 'Floating point 32-bit',
-      pyesedb.column_types.DOUBLE_64BIT: 'Floating point 64-bit',
-      pyesedb.column_types.DATE_TIME: 'Filetime',
-      pyesedb.column_types.BINARY_DATA: 'Binary data',
-      pyesedb.column_types.TEXT: 'Text',
-      pyesedb.column_types.LARGE_BINARY_DATA: 'Large binary data',
-      pyesedb.column_types.LARGE_TEXT: 'Large text',
-      pyesedb.column_types.SUPER_LARGE_VALUE: 'Super large value',
-      pyesedb.column_types.INTEGER_32BIT_UNSIGNED: 'Integer 32-bit unsigned',
-      pyesedb.column_types.INTEGER_64BIT_SIGNED: 'Integer 64-bit signed',
-      pyesedb.column_types.GUID: 'GUID',
-      pyesedb.column_types.INTEGER_16BIT_UNSIGNED: 'Integer 16-bit unsigned',
+      pyesedb.column_types.NULL: u'Null',
+      pyesedb.column_types.BOOLEAN: u'Boolean',
+      pyesedb.column_types.INTEGER_8BIT_UNSIGNED: u'Integer 8-bit unsigned',
+      pyesedb.column_types.INTEGER_16BIT_SIGNED: u'Integer 16-bit signed',
+      pyesedb.column_types.INTEGER_32BIT_SIGNED: u'Integer 32-bit signed',
+      pyesedb.column_types.CURRENCY: u'Currency',
+      pyesedb.column_types.FLOAT_32BIT: u'Floating point 32-bit',
+      pyesedb.column_types.DOUBLE_64BIT: u'Floating point 64-bit',
+      pyesedb.column_types.DATE_TIME: u'Filetime',
+      pyesedb.column_types.BINARY_DATA: u'Binary data',
+      pyesedb.column_types.TEXT: u'Text',
+      pyesedb.column_types.LARGE_BINARY_DATA: u'Large binary data',
+      pyesedb.column_types.LARGE_TEXT: u'Large text',
+      pyesedb.column_types.SUPER_LARGE_VALUE: u'Super large value',
+      pyesedb.column_types.INTEGER_32BIT_UNSIGNED: u'Integer 32-bit unsigned',
+      pyesedb.column_types.INTEGER_64BIT_SIGNED: u'Integer 64-bit signed',
+      pyesedb.column_types.GUID: u'GUID',
+      pyesedb.column_types.INTEGER_16BIT_UNSIGNED: u'Integer 16-bit unsigned',
   }
 
   ESEDB_COLUMN_TYPE_IDENTIFIERS = {
-      pyesedb.column_types.NULL: 'JET_coltypNil',
-      pyesedb.column_types.BOOLEAN: 'JET_coltypBit',
-      pyesedb.column_types.INTEGER_8BIT_UNSIGNED: 'JET_coltypUnsignedByte',
-      pyesedb.column_types.INTEGER_16BIT_SIGNED: 'JET_coltypShort',
-      pyesedb.column_types.INTEGER_32BIT_SIGNED: 'JET_coltypLong',
-      pyesedb.column_types.CURRENCY: 'JET_coltypCurrency',
-      pyesedb.column_types.FLOAT_32BIT: 'JET_coltypIEEESingle',
-      pyesedb.column_types.DOUBLE_64BIT: 'JET_coltypIEEEDouble',
-      pyesedb.column_types.DATE_TIME: 'JET_coltypDateTime',
-      pyesedb.column_types.BINARY_DATA: 'JET_coltypBinary',
-      pyesedb.column_types.TEXT: 'JET_coltypText',
-      pyesedb.column_types.LARGE_BINARY_DATA: 'JET_coltypLongBinary',
-      pyesedb.column_types.LARGE_TEXT: 'JET_coltypLongText',
-      pyesedb.column_types.SUPER_LARGE_VALUE: 'JET_coltypSLV',
-      pyesedb.column_types.INTEGER_32BIT_UNSIGNED: 'JET_coltypUnsignedLong',
-      pyesedb.column_types.INTEGER_64BIT_SIGNED: 'JET_coltypLongLong',
-      pyesedb.column_types.GUID: 'JET_coltypGUID',
-      pyesedb.column_types.INTEGER_16BIT_UNSIGNED: 'JET_coltypUnsignedShort',
+      pyesedb.column_types.NULL: u'JET_coltypNil',
+      pyesedb.column_types.BOOLEAN: u'JET_coltypBit',
+      pyesedb.column_types.INTEGER_8BIT_UNSIGNED: u'JET_coltypUnsignedByte',
+      pyesedb.column_types.INTEGER_16BIT_SIGNED: u'JET_coltypShort',
+      pyesedb.column_types.INTEGER_32BIT_SIGNED: u'JET_coltypLong',
+      pyesedb.column_types.CURRENCY: u'JET_coltypCurrency',
+      pyesedb.column_types.FLOAT_32BIT: u'JET_coltypIEEESingle',
+      pyesedb.column_types.DOUBLE_64BIT: u'JET_coltypIEEEDouble',
+      pyesedb.column_types.DATE_TIME: u'JET_coltypDateTime',
+      pyesedb.column_types.BINARY_DATA: u'JET_coltypBinary',
+      pyesedb.column_types.TEXT: u'JET_coltypText',
+      pyesedb.column_types.LARGE_BINARY_DATA: u'JET_coltypLongBinary',
+      pyesedb.column_types.LARGE_TEXT: u'JET_coltypLongText',
+      pyesedb.column_types.SUPER_LARGE_VALUE: u'JET_coltypSLV',
+      pyesedb.column_types.INTEGER_32BIT_UNSIGNED: u'JET_coltypUnsignedLong',
+      pyesedb.column_types.INTEGER_64BIT_SIGNED: u'JET_coltypLongLong',
+      pyesedb.column_types.GUID: u'JET_coltypGUID',
+      pyesedb.column_types.INTEGER_16BIT_UNSIGNED: u'JET_coltypUnsignedShort',
   }
 
   # TODO: read table and index overlays from file.
@@ -211,7 +196,7 @@ def Main():
 
   # TODO: determine database type and set page_summary?
 
-  page_summary = 'Test'
+  page_summary = u'Test'
 
   output_writer.WritePageHeader(page_summary)
 
@@ -221,7 +206,7 @@ def Main():
     for esedb_column in esedb_table.columns:
       output_writer.WriteColumn(
           esedb_column.identifier, esedb_column.name,
-          ESEDB_COLUMN_TYPE_DESCRIPTIONS.get(esedb_column.type, 'UNKNOWN'))
+          ESEDB_COLUMN_TYPE_DESCRIPTIONS.get(esedb_column.type, u'UNKNOWN'))
 
     output_writer.WriteTableFooter()
 
